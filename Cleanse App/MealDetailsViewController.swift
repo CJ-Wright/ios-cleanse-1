27ae3d3ca5b2f66c5ac542dc0fa88ade33e0fc5c
Erik Karlsson <Erikdkarlsson8@gmail.com>
2016-06-05 18:07:46 +0200

    Trying to get the format of the recipes to appear right. Need to research how to embed a tableview inside of a regular stack view.

--- Cleanse App/MealDetailsViewController.swift
+++ Cleanse App/MealDetailsViewController.swift
@@ -8,19 +8,25 @@
 
 import UIKit
 
-class MealDetailsViewController: UIViewController {
+class MealDetailsViewController: UIViewController, UITableViewDelegate {
     
+    // MARK: - Data Members
     @IBOutlet var recipeNameLabel: UILabel!
     @IBOutlet var mealImage: UIImageView!
     @IBOutlet var recipeDescription: UILabel!
     
+    var loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        recipeDescription.text = loremIpsum
     }
     
     // MARK: - Label Methods
     func addIngredient(ingredient: String) {
 
     }
+    
+    // MARK: - TableView Methods
+    
 }

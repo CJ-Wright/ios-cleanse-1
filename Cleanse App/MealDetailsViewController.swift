cd02bcd2874791efae9e30bb049266cc7967c5f8
Erik Karlsson <Erikdkarlsson8@gmail.com>
2016-06-12 19:02:16 +0200

    Made it possible to display a list in the text label. Working on loading the information of the recipes from a json format. Once it will successfully load the json, I will need to have Anthony load the data through an API.

--- Cleanse App/MealDetailsViewController.swift
+++ Cleanse App/MealDetailsViewController.swift
@@ -14,12 +14,27 @@ class MealDetailsViewController: UIViewController, UITableViewDelegate {
     @IBOutlet var recipeNameLabel: UILabel!
     @IBOutlet var mealImage: UIImageView!
     @IBOutlet var recipeDescription: UILabel!
+    @IBOutlet var ingredientsListLabel: UILabel!
     
     var loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     
+    var ingredients: [String] = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ingredientsListLabel.text = ""
+        
+        recipeDescription.numberOfLines = loremIpsum.characters.count / 10
+        
+        for item in ingredients {
+            ingredientsListLabel.text! += "• " + item + "\n"
+            ingredientsListLabel.numberOfLines += 1
+        }
+        
+        
         recipeDescription.text = loremIpsum
+        
+        loadJSON()
     }
     
     // MARK: - Label Methods
@@ -29,4 +44,13 @@ class MealDetailsViewController: UIViewController, UITableViewDelegate {
     
     // MARK: - TableView Methods
     
+    // MARK: - Data Methods
+    func loadJSON(){
+        if let path = NSBundle.mainBundle().pathForResource("recipes", ofType: "json") {
+            print("Json successfully loaded!")
+            print(path)
+        } else {
+            print("Failed!")
+        }
+    }
 }

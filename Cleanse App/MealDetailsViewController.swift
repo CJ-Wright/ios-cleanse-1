58431e7b0f82690e14a42f06af4a53b7c19411b6
Erik Karlsson <Erikdkarlsson8@gmail.com>
2016-06-24 12:30:49 -0700

    Trying to place a stacked view inside of a scroll view but the stacked view doesn't want to stick with the constraints provide and shrinks to a smaller size.

--- Cleanse App/MealDetailsViewController.swift
+++ Cleanse App/MealDetailsViewController.swift
@@ -18,33 +18,34 @@ class MealDetailsViewController: UIViewController, UITableViewDelegate {
     
     // Stores the JSON representation of the recipes.
     var recipesJSON: AnyObject!
+    var default_recipe: Recipe!
     
-    var instructions = "Preheat the oven to 375 degrees Fahrenheit. In a large pot combine the broth, onion," +
-    "carrots, celery, and 1 tablespoon of the parsley (or cilantro), and garlic. Add 2 cups of" +
-    "water and bring to a boil. Add the rice and bring back to a boil. Cover and simmer for" +
-    "25 minutes. Remove the lid and simmer for 5 more minutes. Set aside." + "\n\n" +
-    "While the rice cooks, put the chicken and broccoli in a mixing bowl. Add the lime" +
-    "juice, ½ teaspoon parsley, salt, and pepper. Mix well until the chicken and broccoli are" +
-    "coated with the ﬂavorings. Transfer the chicken and broccoli mixture to a baking pan," +
-    "spreading it evenly across the bottom with a spatula. Bake for 30 to 35 minutes." + "\n\n" +
-    "Remove the chicken from the oven and allow to cool. Divide chicken and broccoli into" +
-    "four equal portions and place each over 1 cup of the rice/veggie mixture. Serve and" +
-    "enjoy. (Don’t hesitate to double this recipe and freeze leftover portions.)"
-    
-    var ingredients: [String] = [
-        "4 cups vegetable or chicken broth", "½ cup chopped red onion", "½ cup chopped carrot", "½ cup chopped celery",
-        "1 tablespoon plus ½ teaspoon chopped parsley or cilantro",
-        "1 teaspoon minced garlic",
-        "1 pound skinless, boneless chicken",
-        "breast, chopped into 2-inch pieces",
-        "4 cups broccoli",
-        "1 tablespoon lime juice",
-        "½ teaspoon sea salt",
-        "½ teaspoon black pepper",
-        "1 cup brown rice"
-    ]
-    
-    var recipeTitle = "Chicken and Broccoli Bowl"
+//    var instructions = "Preheat the oven to 375 degrees Fahrenheit. In a large pot combine the broth, onion," +
+//    "carrots, celery, and 1 tablespoon of the parsley (or cilantro), and garlic. Add 2 cups of" +
+//    "water and bring to a boil. Add the rice and bring back to a boil. Cover and simmer for" +
+//    "25 minutes. Remove the lid and simmer for 5 more minutes. Set aside." + "\n\n" +
+//    "While the rice cooks, put the chicken and broccoli in a mixing bowl. Add the lime" +
+//    "juice, ½ teaspoon parsley, salt, and pepper. Mix well until the chicken and broccoli are" +
+//    "coated with the ﬂavorings. Transfer the chicken and broccoli mixture to a baking pan," +
+//    "spreading it evenly across the bottom with a spatula. Bake for 30 to 35 minutes." + "\n\n" +
+//    "Remove the chicken from the oven and allow to cool. Divide chicken and broccoli into" +
+//    "four equal portions and place each over 1 cup of the rice/veggie mixture. Serve and" +
+//    "enjoy. (Don’t hesitate to double this recipe and freeze leftover portions.)"
+//    
+//    var ingredients: [String] = [
+//        "4 cups vegetable or chicken broth", "½ cup chopped red onion", "½ cup chopped carrot", "½ cup chopped celery",
+//        "1 tablespoon plus ½ teaspoon chopped parsley or cilantro",
+//        "1 teaspoon minced garlic",
+//        "1 pound skinless, boneless chicken",
+//        "breast, chopped into 2-inch pieces",
+//        "4 cups broccoli",
+//        "1 tablespoon lime juice",
+//        "½ teaspoon sea salt",
+//        "½ teaspoon black pepper",
+//        "1 cup brown rice"
+//    ]
+//    
+//    var recipeTitle = "Chicken and Broccoli Bowl"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,38 +57,18 @@ class MealDetailsViewController: UIViewController, UITableViewDelegate {
         mealImage.layer.borderColor = UIColor .whiteColor().CGColor
         
         ingredientsListLabel.text = ""
+        default_recipe.init_defaults()
         
-        for item in ingredients {
+        for item in default_recipe.ingredients {
             let leftAlignment = NSTextAlignment.Left
             ingredientsListLabel.textAlignment = leftAlignment
             ingredientsListLabel.text! += "• " + item + "\n"
             ingredientsListLabel.numberOfLines += 1
         }
         
-        recipeDescription.text = instructions
-//        recipeDescription.font = recipeDescription.font?.fontWithSize(200)
+        recipeDescription.text = default_recipe.instructions
         recipeDescription.font = .systemFontOfSize(16)
         
     }
     
-    // MARK: - Label Methods
-    func addIngredient(ingredient: String) {
-        
-    }
-    
-    // MARK: - TableView Methods
-    
-    // MARK: - Data Methods
-    func recipesFromJSON(data: NSData){
-//        let path = NSBundle.mainBundle().URLForResource("recipes", withExtension: "json")
-//        let data = NSData(contentsOfURL: path!)
-//
-//        do {
-//            let jsonObject: AnyObject = try NSJSONSerialization.JSONObjectWithData(data, options: [])
-//            print(jsonObject)
-//            print("Loaded JSON!")
-//        } catch let error {
-//            return .Failure(error)
-//        }
-    }
 }

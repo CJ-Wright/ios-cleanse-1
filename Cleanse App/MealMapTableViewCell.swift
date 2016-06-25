89d12e281e28fdd4f2ada5ad6941583b45f6d536
Erik Karlsson <Erikdkarlsson8@gmail.com>
2016-06-24 17:30:53 -0700

    Got the meal mapper table view to work correctly. It will load custom images and labels. Next is for it dynamically go to the detail view when at item is selected.

---
+++ Cleanse App/MealMapTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MealMappTableViewCell.swift
+//  Cleanse App
+//
+//  Created by Erik Karlsson on 6/24/16.
+//  Copyright © 2016 Nano Nimbus. All rights reserved.
+//
+
+import UIKit
+
+class MealMapTableViewCell: UITableViewCell {
+
+    @IBOutlet var mealMapNameLabel: UILabel!
+    
+    @IBOutlet var mealMapImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

6509c25d3e9a5b190cf4578023d0e9da7be1c880
edkarlsson <Erikdkarlsson8@gmail.com>
2016-07-24 18:55:10 -0700

    Committing the DjoserAPI file before I continue working on the home screen.

--- Cleanse App/DjoserAPI.swift
+++ Cleanse App/DjoserAPI.swift
@@ -19,7 +19,8 @@ enum AuthToken {
 
 struct DjoserAPI {
     // Development environment
-    private static let baseURLString = "http://localhost:8000"
+//    private static let baseURLString = "http://localhost:8000"
+    private static let baseURLString = "http://dev-djoser-postgres.cjv0rqkpv8ys.us-west-1.rds.amazonaws.com:8000"
     
     // This function will return the url endpoint of the API server which is used for storing the Recipes and the Meal Plans
     private static func djoserURL(method method: Method, parameters: [String:String]?) -> NSURL {
@@ -93,30 +94,5 @@ struct DjoserAPI {
         
         return components.URL!
     }
-    
-    static func authTokenFromJSONData(data: NSData) -> RegisterResult {
-        do {
-            // Attempt to convert the json object into an AnyObject
-            let jsonObject:AnyObject = try NSJSONSerialization.JSONObjectWithData(data, options: [])
-            
-            // Create an array of recipes to store the converted JSON Objects
-            var token: AuthToken
-            
-            
-            
-            if jsonObject.count > 0 {
-                // We weren't able to parse any of the photso
-                // Maybe the JSON format for photos has changed
-                print("Failed to register the user")
-                return .Failure(DeploydError.InvalidJSONData)
-            }
-            
-            return .Success(token)
-        } catch let error {
-            return .Failure(error)
-        }
-   
-    }
-    
-    
+       
 }

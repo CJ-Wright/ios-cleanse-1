fd89da05a09c997e0f0e13222c6f8b822e7a12c4
edkarlsson <Erikdkarlsson8@gmail.com>
2016-09-10 16:46:27 -0700

    Got the Food list to display properly and expandable woohoo. Going onto changing recipes

---
+++ Cleanse App/CustomCell.swift
@@ -0,0 +1,56 @@
+//
+//  ChangeRecipeTableViewController.swift
+//  Cleanse App
+//
+//  Created by Erik Karlsson on 9/10/16.
+//  Copyright © 2016 Nano Nimbus. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell, UITextFieldDelegate {
+
+    // MARK: IBOutlet Properties
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    // MARK: Constants
+    
+    let bigFont = UIFont(name: "Avenir-Book", size: 13.0)
+    
+    let smallFont = UIFont(name: "Avenir-Light", size: 12.0)
+    
+    let primaryColor = UIColor.blackColor()
+    
+    let secondaryColor = UIColor.lightGrayColor()
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        if textLabel != nil {
+            textLabel?.font = bigFont
+            textLabel?.textColor = primaryColor
+        }
+        
+        if detailTextLabel != nil {
+            detailTextLabel?.font = smallFont
+            detailTextLabel?.textColor = secondaryColor
+        }
+        
+        if textField != nil {
+            textField.font = bigFont
+            textField.delegate = self
+        }
+        
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Cleanse App/FoodListTableViewController.swift
+++ Cleanse App/FoodListTableViewController.swift
@@ -9,10 +9,54 @@
 import UIKit
 
 class FoodListTableViewController: UITableViewController {
+    
+    
+    var cellDescriptors: NSMutableArray!
+    var visibleRowsPerSection = [[Int]]()
 
+    func loadCellDescriptors(){
+        if let path = NSBundle.mainBundle().pathForResource("FoodListCellDescriptor", ofType: "plist"){
+            cellDescriptors = NSMutableArray(contentsOfFile: path)
+            getIndicesOfVisibleRows()
+            tableView.reloadData()
+        }
+    }
+    func getIndicesOfVisibleRows() {
+        visibleRowsPerSection.removeAll()
+        
+        for currentSectionCells in cellDescriptors {
+            var visibleRows = [Int]()
+            
+            for row in 0...((currentSectionCells as! [[String: AnyObject]]).count - 1) {
+                if currentSectionCells[row]["isVisible"] as! Bool == true {
+                    visibleRows.append(row)
+                }
+            }
+            
+            visibleRowsPerSection.append(visibleRows)
+        }
+    }
+    
+    func getCellDescriptorForIndexPath(indexPath: NSIndexPath) -> [String: AnyObject] {
+        let indexOfVisibleRow = visibleRowsPerSection[indexPath.section][indexPath.row]
+        let cellDescriptor = cellDescriptors[indexPath.section][indexOfVisibleRow] as! [String: AnyObject]
+        return cellDescriptor
+    }
+    
+    func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView(frame: CGRectZero)
+    
+        tableView.registerNib(UINib(nibName: "NormalCell", bundle: nil), forCellReuseIdentifier: "idCellNormal")
+        tableView.registerNib(UINib(nibName: "TextfieldCell", bundle: nil), forCellReuseIdentifier: "idCellTextfield")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.configureTableView()
+        loadCellDescriptors()
+        print(cellDescriptors)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -28,67 +72,78 @@ class FoodListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        if cellDescriptors != nil {
+            return cellDescriptors.count
+        }
+        else {
+            return 0
+        }
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return visibleRowsPerSection[section].count
     }
-
-    /*
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Food List"
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let currentCellDescriptor = getCellDescriptorForIndexPath(indexPath)
+        
+        switch currentCellDescriptor["cellIdentifier"] as! String {
+        case "idCellNormal":
+            return 60.0
+            
+        case "idCellDatePicker":
+            return 270.0
+            
+        default:
+            return 44.0
+        }
+    }
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-        // Configure the cell...
-
+        let currentCellDescriptor = getCellDescriptorForIndexPath(indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier(currentCellDescriptor["cellIdentifier"] as! String, forIndexPath: indexPath) as! CustomCell
+        
+        if currentCellDescriptor["cellIdentifier"] as! String == "idCellNormal" {
+            if let primaryTitle = currentCellDescriptor["primaryTitle"] {
+                cell.textLabel?.text = primaryTitle as? String
+            }
+            
+            if let secondaryTitle = currentCellDescriptor["secondaryTitle"] {
+                cell.detailTextLabel?.text = secondaryTitle as? String
+            }
+        }
+        else if currentCellDescriptor["cellIdentifier"] as! String == "idCellTextfield" {
+            cell.textField.placeholder = currentCellDescriptor["primaryTitle"] as? String
+        }
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let indexOfTappedRow = visibleRowsPerSection[indexPath.section][indexPath.row]
+        
+        if cellDescriptors[indexPath.section][indexOfTappedRow]["isExpandable"] as! Bool == true {
+            var shouldExpandAndShowSubRows = false
+            if cellDescriptors[indexPath.section][indexOfTappedRow]["isExpanded"] as! Bool == false {
+                // In this case the cell should expand.
+                shouldExpandAndShowSubRows = true
+            }
+            
+            cellDescriptors[indexPath.section][indexOfTappedRow].setValue(shouldExpandAndShowSubRows, forKey: "isExpanded")
+            
+            for i in (indexOfTappedRow + 1)...(indexOfTappedRow + (cellDescriptors[indexPath.section][indexOfTappedRow]["additionalRows"] as! Int)) {
+                cellDescriptors[indexPath.section][i].setValue(shouldExpandAndShowSubRows, forKey: "isVisible")
+            }
+        }
+        getIndicesOfVisibleRows()
+        tableView.reloadSections(NSIndexSet(index: indexPath.section), withRowAnimation: UITableViewRowAnimation.Fade)
+    }
+    
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+        return false
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
